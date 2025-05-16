47d78763f9e696f3a2bae5c2cc61bbf03b27805a
Rennan Rentz Lopes da Silva <rennanrentz@gmail.com>
2025-05-16 20:02:12 -0300

    Adiciona funcao para mostrar aba e icones para alterar o dark mode

--- cpf.js
+++ cpf.js
@@ -1,3 +1,13 @@
 const toggleTheme = document.getElementById('toggleTheme');
 const historicoLista = document.getElementById('historico-lista');
 const historico = [];
+
+toggleTheme.addEventListener('click', () => {
+  document.body.classList.toggle('light');
+  toggleTheme.textContent = document.body.classList.contains('light') ? '🌞' : '🌙';
+});
+
+function mostrarAba(tipo) {
+  document.querySelectorAll('.tab-content').forEach(aba => aba.classList.remove('active'));
+  document.getElementById(tipo).classList.add('active');
+}
